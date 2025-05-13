efe16a29a1e1df928a29bd502b5e5a30f2b71997
andzekausk <s23kausandz@venta.lv>
2025-05-13 11:57:16 +0300

    Added manual item code entry when scanning

--- frontend/src/views/QRTest.vue
+++ frontend/src/views/QRTest.vue
@@ -12,8 +12,16 @@
       <p><strong>Atrašanās vieta:</strong> {{ itemData.room || '-' }}</p>
     </div>
 
-    <button @click="stopScanner" v-if="scannerRunning">Beigt skenēšanu</button>
-    <button @click="resumeScanner" v-else-if="scannedCode">Turpināt</button>
+    <div v-if="scannerRunning">
+      <input v-model="manualCode" @keyup.enter="handleManualSearch" placeholder="Ievadi inventāra numuru" />
+      <button @click="handleManualSearch">Meklēt</button>
+    </div>
+
+    <div>
+      <!-- <button @click="stopScanner" v-if="scannerRunning">Beigt skenēšanu</button> -->
+      <button @click="resumeScanner" v-if="scannedCode">Turpināt</button>
+    </div>
+
   </div>
 </template>
 
@@ -27,28 +35,29 @@ import {
 import api from "../services/api";
 
 const scannedCode = ref(null)
+const manualCode = ref('')
 const scannerRunning = ref(false)
 const itemData = ref(null)
 
 let html5QrCode = null
 const qrRegionId = "reader"
 
+const config = {
+  fps: 10,
+  supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+  formatsToSupport: [
+    // Html5QrcodeSupportedFormats.QR_CODE,
+    Html5QrcodeSupportedFormats.CODE_128,
+    // Html5QrcodeSupportedFormats.EAN_13,
+    // Html5QrcodeSupportedFormats.UPC_A,
+    // Html5QrcodeSupportedFormats.UPC_E,
+    Html5QrcodeSupportedFormats.CODE_39
+  ]
+}
+
 const startScanner = async () => {
   try {
     html5QrCode = new Html5Qrcode(qrRegionId)
-
-    const config = {
-      fps: 10,
-      supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
-      formatsToSupport: [
-        // Html5QrcodeSupportedFormats.QR_CODE,
-        // Html5QrcodeSupportedFormats.CODE_128,
-        // Html5QrcodeSupportedFormats.EAN_13,
-        // Html5QrcodeSupportedFormats.UPC_A,
-        // Html5QrcodeSupportedFormats.UPC_E,
-        Html5QrcodeSupportedFormats.CODE_39
-      ]
-    }
 
     await html5QrCode.start(
       { facingMode: "environment" },
@@ -91,7 +100,26 @@ const stopScanner = async () => {
 const resumeScanner = async () => {
   itemData.value = null
   scannedCode.value = null
+  manualCode.value = ''
   await startScanner()
+}
+
+const handleManualSearch = async () => {
+  const code = manualCode.value.trim()
+  if (!code) return
+
+  scannedCode.value = code
+  try {
+    const { data } = await api.get(`/items/item_number/${code}`)
+    itemData.value = data
+    await stopScanner()
+  } catch (err) {
+    if (err.response?.status === 404) {
+      itemData.value = null
+    } else {
+      console.error("Server error:", err)
+    }
+  }
 }
 
 onMounted(() => {
