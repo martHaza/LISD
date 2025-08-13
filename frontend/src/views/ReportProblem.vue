b1579936827d5f57cc177cd6f02ed8a37fed68f7
martHaza <martazapacka17@gmail.com>
2025-08-13 15:26:13 +0300

    Iesākts ReportProblem.vue

---
+++ frontend/src/views/ReportProblem.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useAuthStore } from "../store/auth";
+import { jwtDecode } from "jwt-decode";
+import api from "../services/api";
+
+const authStore = useAuthStore();
+const items = ref([]);
+const selectedItems = ref([]);
+const title = ref("");
+const description = ref("");
+
+const fetchItems = async () => {
+    try {
+        const response = await api.get(`/items`);
+        items.value = response.data;
+    } catch (error) {
+        console.error("Error fetching items", error);
+    }
+};
+
+const toggleItemSelection = (itemId) => {
+    const index = selectedItems.value.indexOf(itemId);
+    if (index === -1) {
+        selectedItems.value.push(itemId);
+    } else {
+        selectedItems.value.splice(index, 1);
+    }
+};
+
+const submitDamageReport = async () => {
+    if (!title.value || !description.value || selectedItems.value.length === 0) {
+        alert("Lūdzu aizpildiet visus laukus un izvēlieties vismaz vienu inventāra vienību.");
+        return;
+    }
+    const userId = jwtDecode(authStore.token)?.user_id;
+        if (!userId) {
+            alert("Nevarēja iegūt lietotāja ID. Lūdzu, ielogojieties vēlreiz!");
+            return;
+        }
+
+    try {
+        await api.post(`/damaged-item`, {
+            user_id: userId,
+            title: title.value,
+            description: description.value,
+            status: "new",
+            inventory_ids: selectedItems.value,
+        });
+
+        alert("Bojājums veiksmīgi pieteikts!");
+        selectedItems.value = [];
+        title.value = "";
+        description.value = "";
+    } catch (error) {
+        console.error("Neizdevās pieteikt bojājumu:", error);
+        alert("Radās kļūda pieteikuma saglabāšanā!");
+    }
+}
+
+onMounted(fetchItems);
+</script>
+
+<template>
+    
+</template>
