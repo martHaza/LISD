f3e83e910f83244c2827d12d397ac882a3edb71d
martHaza <martazapacka17@gmail.com>
2025-08-15 14:32:35 +0300

    Pielabots vite.config.js, lai nerādītos errori console

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,14 +24,18 @@ export default defineConfig({
         background_color: "#ffffff",
         theme_color: "#42b883",
         icons: [
-          { src: "/pwa-192x192.png", sizes: "192x192", type: "image/png" },
-          { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png" },
-          { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" }
+          { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "pwa-512x512.png", sizes: "512x512", type: "image/png" },
+          { src: "pwa-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" }
         ]
       },
       workbox: {
         cleanupOutdatedCaches: true,
-        globPatterns: ["**/*.{js,css,html,png,svg,webp,woff2}"]
+        globPatterns: ["**/*.{js,css,html,png,svg,webp,woff2}"],
+        navigateFallback: '/lisd/index.html',        
+    navigateFallbackAllowlist: [
+      /^\/lisd\/.*/                              
+    ]
       }
     })
   ]
